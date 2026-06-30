5a918b8eb536e508ca8ace19e39d6b8d2d915588
Jukka Palm <jukka.palm@hotmail.fi>
2026-06-30 20:03:19 +0300

    Lisatty grid tietorakenne

--- js/main.js
+++ js/main.js
@@ -1,7 +1,34 @@
 const canvas = document.getElementById('game-canvas');
 const ctx = canvas.getContext('2d');
 
+// Ruutujen määrä  esim. 10x10
 const GRID_SIZE = 10;
+
+// Ruudukon tila tallennetaan 2D-taulukkoon.
+// Jokainen soli on joku seuraavista: 'empty', 'wall', 'start', 'end', 'visited', 'path'
+// Tämä pitää datan (mitä solussa on) erillään piirtämisestä
+// jolloin ruudukon tilaa voi muokata ja sen mukaan vain piirtää uudelleen
+let grid = [];
+
+// Luodaan tyhjä ruudukko, jossa jokainen solu on aina alussa 'empty'
+function createEmptyGrid() {
+    const newGrid = [];
+
+    for (let row = 0; row < GRID_SIZE; row++) {
+        const currentRow = [];
+
+        for (let col = 0; col < GRID_SIZE; col++) {
+            currentRow.push('empty');
+        }
+
+        newGrid.push(currentRow);
+    }
+
+    return newGrid;
+}
+
+// Alustetaan grid heti kun sivu latautuu
+grid = createEmptyGrid();
 
 function initGame() {
     canvas.width = canvas.offsetWidth;
@@ -10,6 +37,7 @@ function initGame() {
     drawGrid();
 }
 
+// Piirtää ruudukon
 function drawGrid() {
     const cellSize = canvas.width / GRID_SIZE;
 
@@ -20,8 +48,21 @@ function drawGrid() {
             const x = col * cellSize;
             const y = row * cellSize;
 
-            // Solun taustaväri
-            ctx.fillStyle = '#0A0A0A';
+            // Valitaan solun väri sen tilan mukaan (grid[row][col]).
+            switch (grid[row][col]) {
+                case 'wall':
+                    ctx.fillStyle = '#333333';
+                    break;
+                case 'visited':
+                    ctx.fillStyle = '#660000';
+                    break;
+                case 'path':
+                    ctx.fillStyle = '#FF8C00';
+                    break;
+                default:
+                    ctx.fillStyle = '#0A0A0A';
+            }
+
             ctx.fillRect(x, y, cellSize, cellSize);
 
             // Ruudukon viivat
